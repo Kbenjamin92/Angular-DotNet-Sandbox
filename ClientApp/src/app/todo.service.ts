49ef353074c3be650a1374d7115ae17446e4528e
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-06-08 23:12:03 -0400

    building todo app to practice

---
+++ ClientApp/src/app/todo.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  public title: string;
+  public description: string;
+
+  constructor() { }
+
+  
+
+}
---
+++ ClientApp/src/app/todo/todo.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { TodoService } from '../todo.service'
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  public titleData;
+  @Input() title = '';
+  @Input() description = '';
+  @Output() onClick = new EventEmitter;
+
+  constructor(public todoData: TodoService) { }
+
+  ngOnInit() {
+  }
+
+  displayData() {
+    this.onClick.emit(this.title = this.titleData);
+    console.log(this.titleData)
+    // this.todoData.description = this.description;
+  }
+
+}
